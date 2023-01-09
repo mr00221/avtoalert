7e950765bd122cbed453819a5ba4ac9252adb8f5
dexstroy <matej.rus7@gmail.com>
2023-01-09 22:40:00 +0100

    BUG fix: Ne nalozi css

--- avtoalert/settings.py
+++ avtoalert/settings.py
@@ -132,3 +132,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
